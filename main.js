c39b2bf56839b20b7dc6829e312c778ee7dc12f1
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-08 20:43:01 -0300

    Estruturando os dados do Modal

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
+
+const Modal = {
+  wrapper: document.querySelector('.modal-wrapper'),
+  message: document.querySelector('.modal .title span'),
+  buttonClose: document.querySelector('.modal button.close'),
+  open() {
+    Modal.wrapper.classList.add('open')
+  },
+  close() {
+    Modal.wrapper.classList.remove('open')
+  }
+}
 
 form.onsubmit = function (e) {
   e.preventDefault()
@@ -14,12 +23,12 @@ form.onsubmit = function (e) {
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
-  modalMessage.innerText = message
-  modalWrapper.classList.add('open')
+  Modal.message.innerText = message
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => {
-  modalWrapper.classList.remove('open')
+Modal.buttonClose.onclick = () => {
+  Modal.close()
 }
 
 function IMC(weight, height) {
