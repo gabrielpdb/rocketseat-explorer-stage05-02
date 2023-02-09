29db8903ea5660c7a1d1010435e80a4ebe4b87a0
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-08 21:18:09 -0300

    Fechando modal pelo teclado

--- js/modal.js
+++ js/modal.js
@@ -13,3 +13,10 @@ export const Modal = {
 Modal.buttonClose.onclick = () => {
   Modal.close()
 }
+
+window.addEventListener('keydown', handleKeyDown)
+function handleKeyDown(event) {
+  if (event.key === 'Escape') {
+    Modal.close()
+  }
+}
