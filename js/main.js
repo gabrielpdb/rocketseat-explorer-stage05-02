8900f77f05385aec906b6edf6f7cc498c55a9cad
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-08 21:28:42 -0300

    Modularizar as funções utilitárias

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
+import { IMC, notNumber } from './utils.js'
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -25,12 +26,4 @@ form.onsubmit = function (e) {
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-function notNumber(value) {
-  return isNaN(value) || value == ''
-}
-
-function IMC(weight, height) {
-  return (weight / (height / 100) ** 2).toFixed(2)
 }
