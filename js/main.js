f6596ba36684693a80a1509513d4fa5c8ee5a791
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-08 21:33:31 -0300

    Refatorando a função de submit, aplicando regras de Clean Code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { Modal } from './modal.js'
 import { AlertError } from './alert-error.js'
-import { IMC, notNumber } from './utils.js'
+import { calculateIMC, notNumber } from './utils.js'
 
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -12,16 +12,20 @@ form.onsubmit = function (e) {
   const weight = inputWeight.value
   const height = inputHeight.value
 
-  const showAlertError = notNumber(weight) || notNumber(height)
+  const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
 
-  if (showAlertError) {
+  if (weightOrHeightIsNotANumber) {
     AlertError.open()
     return
   }
 
   AlertError.close()
 
-  const result = IMC(weight, height)
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
   const message = `Seu IMC é de ${result}`
 
   Modal.message.innerText = message
