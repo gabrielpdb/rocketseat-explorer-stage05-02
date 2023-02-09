4db958fe861892fbec62fa4a9983d43b8d57264a
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-08 21:01:00 -0300

    Modularizando o Modal popup

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
+import { Modal } from './modal.js'
+
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-  wrapper: document.querySelector('.modal-wrapper'),
-  message: document.querySelector('.modal .title span'),
-  buttonClose: document.querySelector('.modal button.close'),
-  open() {
-    Modal.wrapper.classList.add('open')
-  },
-  close() {
-    Modal.wrapper.classList.remove('open')
-  }
-}
 
 form.onsubmit = function (e) {
   e.preventDefault()
@@ -25,10 +15,6 @@ form.onsubmit = function (e) {
 
   Modal.message.innerText = message
   Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-  Modal.close()
 }
 
 function IMC(weight, height) {
